f951786081c43959f81a1cc64c1344c7a94131c2
Tom Palmer <remlapmot@hotmail.com>
2021-06-21 12:06:53 +0100

    Create onesamplemr-package.R

---
+++ R/onesamplemr-package.R
@@ -0,0 +1,11 @@
+#' OneSampleMR: Use functions for one-sample Mendelian randomization
+#' and instrumental variables analyses
+#'
+#' Some useful functions for one-sample Mendelian randomization
+#' and instrumental variables analyses.
+#'
+#' @name OneSampleMR-package
+#' @docType package
+#' @aliases onesamplemr OneSampleMR
+#'
+NULL
