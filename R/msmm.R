197eff77bb521e19ec06636d386238c212c417eb
Tom Palmer <remlapmot@hotmail.com>
2021-06-28 12:38:07 +0100

    amend ref

--- R/msmm.R
+++ R/msmm.R
@@ -11,7 +11,8 @@
 #'    * `"tslsalt"` the alternative TSLS method of Clarke et al. (2015)
 #' @references Clarke PS, Palmer TM Windmeijer F. Estimating structural
 #' mean models with multiple instrumental variables using the
-#' Generalised Method of Moments.
+#' Generalised Method of Moments. Statistical Science, 2015, 30, 1,
+#' 96-117, \doi{10.1214/14-STS503}.
 #' @export
 msmm <- function(formula, estmethod = "gmm") {
 
