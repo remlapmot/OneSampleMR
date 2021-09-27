35f3244b68eb74d2e0f312bde7dc8c77b58601dc
Tom Palmer <remlapmot@hotmail.com>
2021-09-27 18:15:53 +0100

    add ref

--- R/onesamplemr-package.R
+++ R/onesamplemr-package.R
@@ -4,6 +4,13 @@
 #' Some useful functions for one-sample Mendelian randomization
 #' and instrumental variables analyses.
 #'
+#' @references
+#' Palmer TM, Sterne JAC, Harbord RM, Lawlor DA, Sheehan NA, Meng S,
+#' Granell R, Davey Smith G, Didelez V.
+#' Instrumental variable estimation of causal risk ratios and causal odds ratios
+#' in Mendelian randomization analyses.
+#' American Journal of Epidemiology, 2011, 173, 12, 1392-1403.
+#' \doi{10.1093/aje/kwr026}
 #' @name OneSampleMR-package
 #' @docType package
 #' @aliases onesamplemr OneSampleMR
