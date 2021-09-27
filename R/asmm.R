40302212a55592af72251a0356ed42f85d90cfec
Tom Palmer <remlapmot@hotmail.com>
2021-09-27 18:16:26 +0100

    add ref

--- R/asmm.R
+++ R/asmm.R
@@ -11,6 +11,13 @@
 #' Generalised Method of Moments. Statistical Science, 2015, 30, 1,
 #' 96-117. \doi{10.1214/14-STS503}
 #'
+#' Palmer TM, Sterne JAC, Harbord RM, Lawlor DA, Sheehan NA, Meng S,
+#' Granell R, Davey Smith G, Didelez V.
+#' Instrumental variable estimation of causal risk ratios and causal odds ratios
+#' in Mendelian randomization analyses.
+#' American Journal of Epidemiology, 2011, 173, 12, 1392-1403.
+#' \doi{10.1093/aje/kwr026}
+#'
 #' Robins JM. The analysis of randomised and
 #' nonrandomised AIDS treatment trials using a new approach to
 #' causal inference in longitudinal studies.
