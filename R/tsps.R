290c81e91b751902b0daf1ad70aec590f184e1e5
Tom Palmer <remlapmot@hotmail.com>
2021-10-20 14:43:15 +0100

    fix typo

--- R/tsps.R
+++ R/tsps.R
@@ -4,7 +4,7 @@
 #' They proceed by fitting a first stage model of the
 #' exposure regressed upon the instruments (and possibly any measured
 #' confounders). From this the predicted values of the exposure are obtained.
-#' A second stage model if then fitted of the outcome regressed upon
+#' A second stage model is then fitted of the outcome regressed upon
 #' the predicted values of the exposure (and possibly measured confounders).
 #'
 #' `tsps()` performs GMM estimation to ensure appropriate standard errors
