4d96f20171c4edffe535b4e19109f618c757bee8
Tom Palmer <remlapmot@hotmail.com>
2021-11-11 11:15:42 +0000

    no need to delete object as no longer in global namespace

--- R/tsri.R
+++ R/tsri.R
@@ -205,7 +205,6 @@ tsri <- function(formula, instruments, data, subset, na.action,
                      xnames = xnames,
                      t0 = t0,
                      link = link)
-  rm(tsri_env, envir = .GlobalEnv)
   class(output) <- append("tsri", class(output))
   output
 }
